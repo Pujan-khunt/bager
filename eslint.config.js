81b6e039e2fdaf10d55d5d1914c3398e090320b1
Pujan-khunt <pujankhunt2412@gmail.com>
2025-04-23 14:15:35 +0530

    chore: configure vite.config to create a dist folder with appropirate file paths of the scripts and create blank scripts

--- eslint.config.js
+++ eslint.config.js
@@ -28,6 +28,8 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"]
     },
   },
 ]
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "popup.html"),
+        background: resolve(__dirname, "src/background/background.js"),
+        content: resolve(__dirname, "src/content/content.js")
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === "background") return "background/[name].js";
+          if (chunkInfo.name === "content") return "content/[name].js";
+          return "[name].js";
+        }
+      }
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true
+  }
+});
