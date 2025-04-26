2ce76fec3d732bf7366737341f3383cd89c1c35c
Pujan-khunt <pujankhunt2412@gmail.com>
2025-04-26 22:01:54 +0530

    fix: fix bug of importing useEffect

--- src/popup/hooks/useVersion.jsx
+++ src/popup/hooks/useVersion.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { sendMessage } from "../utils/chromeAPI";
 
 export function useVersion() {
-  const [version, setVersion] = useState(() => {
-    async function fetchVersion() {
-      const response = await sendMessage({ type: "GET_VERSION" })
-      setVersion(response.version)
-    };
+  const [version, setVersion] = useState("0.0.0");
+
+  useEffect(() => {
+    const fetchVersion = async () => {
+      const responseObj = await sendMessage({ type: "GET_VERSION" });
+      setVersion(responseObj.version)
+    }
 
     fetchVersion();
-  });
+  })
 
   return version;
 }
